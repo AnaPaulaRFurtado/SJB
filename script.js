c070ba2bba07ffbaea9424b587565343ba6fa94e
Ana Paula Furtado <furtadorodriguesanapaula@gmail.com>
2025-09-15 18:21:30 -0300

    Menu
    responsividade

--- script.js
+++ script.js
@@ -1,23 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-  
-  document.querySelectorAll(".tab-btn").forEach(button => {
-    button.addEventListener("click", () => {
-      const tabId = button.dataset.tab;
-      document.querySelectorAll(".tab-btn").forEach(btn => btn.classList.remove("active"));
-      document.querySelectorAll(".tab-content").forEach(tab => tab.classList.remove("active"));
-      button.classList.add("active");
-      const el = document.getElementById(tabId);
-      if (el) el.classList.add("active");
-    });
-  });
-
-
-  const navLinks = document.querySelectorAll("nav a");
-
-  navLinks.forEach(link => {
-    link.addEventListener("click", function () {
-      navLinks.forEach(a => a.classList.remove("active"));
-      this.classList.add("active");
+  const links = document.querySelectorAll("nav a");
+  links.forEach(link => {
+    link.addEventListener("click", () => {
+      links.forEach(a => a.classList.remove("active"));
+      link.classList.add("active");
+      link.blur(); 
     });
   });
 });
+
