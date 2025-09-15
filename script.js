bcc35873f5f05ac0fc4dcf21e3f25896490e7dbb
Ana Paula Furtado <furtadorodriguesanapaula@gmail.com>
2025-09-15 18:31:30 -0300

    Js

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-document.addEventListener('DOMContentLoaded', () => {
-  
-  document.querySelectorAll(".tab-btn").forEach(button => {
-    button.addEventListener("click", () => {
-      const tabId = button.dataset.tab;
-      document.querySelectorAll(".tab-btn").forEach(btn => btn.classList.remove("active"));
-      document.querySelectorAll(".tab-content").forEach(tab => tab.classList.remove("active"));
-      button.classList.add("active");
-      const el = document.getElementById(tabId);
-      if (el) el.classList.add("active");
-    });
+
+document.querySelectorAll(".tab-btn").forEach(button => {
+  button.addEventListener("click", () => {
+    const tabId = button.dataset.tab;
+
+    document.querySelectorAll(".tab-btn").forEach(btn => btn.classList.remove("active"));
+    document.querySelectorAll(".tab-content").forEach(tab => tab.classList.remove("active"));
+
+    button.classList.add("active");
+    document.getElementById(tabId).classList.add("active");
   });
-})
+});
