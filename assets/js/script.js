0f586a1b73ac91cc6af07a73ce5bac7c065959fd
Ana Paula Furtado <furtadorodriguesanapaula@gmail.com>
2025-12-07 11:04:58 -0300

    Repertório 2 domingo do advento

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+
 document.querySelectorAll(".tab-btn").forEach(button => {
   button.addEventListener("click", () => {
     const tabId = button.dataset.tab;
@@ -25,3 +26,35 @@ document.querySelectorAll(".toggleLetra").forEach(btn => {
   });
 });
 
+document.querySelectorAll(".menu-item").forEach(btn => {
+  btn.addEventListener("click", () => {
+    const alvo = btn.dataset.target;
+
+    document.getElementById("menu-lista").classList.add("hidden");
+    document.getElementById("conteudo").classList.remove("hidden");
+
+    document.querySelectorAll(".canto").forEach(sec => sec.classList.add("hidden"));
+    document.getElementById(alvo).classList.remove("hidden");
+
+    document.getElementById("btn-voltar").style.display = "block";
+  });
+});
+
+document.getElementById("btn-voltar").addEventListener("click", () => {
+  document.getElementById("conteudo").classList.add("hidden");
+  document.getElementById("menu-lista").classList.remove("hidden");
+
+  document.getElementById("btn-voltar").style.display = "none";
+});
+
+
+document.getElementById("btn-voltar-header").addEventListener("click", () => {
+  if (!document.getElementById("menu-lista").classList.contains("hidden")) {
+    window.location.href = "index.html";
+    return;
+  }
+
+  document.getElementById("conteudo").classList.add("hidden");
+  document.getElementById("menu-lista").classList.remove("hidden");
+  document.getElementById("btn-voltar").style.display = "none";
+});
